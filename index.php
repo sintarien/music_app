a6f6c5b8455743415c98d5c87ddd0918fe2a0dd1
sintarien <sintatonikku@yahoo.co.jp>
2021-05-18 21:34:35 +0900

    メッセージ返信機能追加

--- index.php
+++ index.php
@@ -23,10 +23,11 @@ if(!empty($_POST)){
   if($_POST['message'] !== ''){
 
 
-    $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, created=NOW()');
+    $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, reply_message_id=?,created=NOW()');
     $message->execute(array(
       $member['id'],
-      $_POST['message']
+      $_POST['message'],
+      $_POST['reply_message_id']
     ));
 
     //POST処理が行われたらもう一度自分自身を呼び出してPOSTの値を初期化する
@@ -36,7 +37,19 @@ if(!empty($_POST)){
 }
 //投稿されたデータをcreatedが新しい物から順に取得
 $posts = $db->query('SELECT m.name, m.user_image, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+
+//返信ボタンがクリックされた場合
+if(isset($_REQUEST['res'])){
+  //返信の処理
+  $response = $db->prepare('SELECT m.name, m.user_image, p. * FROM members m, posts p WHERE m.id=p.member_id AND p.id=?');
+  $response->execute(array($_REQUEST['res']));
+
+  //指定されたメッセージのidを$messageに格納する
+  $table = $response->fetch();
+  $message = '@' . $table['name'] . ' ' . $table['message'];
+}
 ?>
+
 
 
 <!DOCTYPE html>
@@ -61,8 +74,8 @@ $posts = $db->query('SELECT m.name, m.user_image, p.* FROM members m, posts p WH
       <dl>
         <dt><?php print(htmlspecialchars($member['name'],ENT_QUOTES)); ?>さん、メッセージをどうぞ</dt>
         <dd>
-          <textarea name="message" cols="50" rows="5"></textarea>
-          <input type="hidden" name="reply_post_id" value="" />
+          <textarea name="message" cols="50" rows="5"><?php print(htmlspecialchars($message, ENT_QUOTES)); ?></textarea>
+          <input type="hidden" name="reply_post_id" value="<?php print(htmlspecialchars($_REQUEST['res'], ENT_QUOTES)); ?>" />
         </dd>
       </dl>
       <div>
@@ -75,10 +88,12 @@ $posts = $db->query('SELECT m.name, m.user_image, p.* FROM members m, posts p WH
 <?php foreach($posts as $post): ?>
     <div class="msg">
     <img src="member_picture/<?php print(htmlspecialchars($post['user_image'], ENT_QUOTES)); ?>" width="48" height="48" alt="<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>" />
-    <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?><span class="name">（<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>）</span>[<a href="index.php?res=">Re</a>]</p>
-    <p class="day"><a href="view.php?id="><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
-<a href="view.php?id=">
-返信元のメッセージ</a>
+    <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?><span class="name">（<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>）</span>[<a href="index.php?res=<?php print(htmlspecialchars($post['id'],ENT_QUOTES)); ?>">Re</a>]</p>
+    <p class="day"><a href="view.php?id=<?php print(htmlspecialchars($post['id'])); ?>"><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
+  <?php if($post['reply_message_id'] > 0): ?>
+   <a href="view.php?id=<?php print(htmlspecialchars($post['reply_message_id	'])); ?>">
+  返信元のメッセージ</a>
+  <?php endif; ?>
 [<a href="delete.php?id="
 style="color: #F33;">削除</a>]
     </p>
